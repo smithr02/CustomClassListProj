1bc04940185e633f623f181baac46dd3df421e85
Ryan Smith <ryan_smith2@hotmail.com>
2020-04-28 00:18:14 -0500

    added 3 test methods for the add method in the custom list.

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest1
@@ -41,11 +43,56 @@ namespace UnitTest1
         }
 
         // what happens if you add multiple things (or add to a CustomList that already has some values)?
+        [TestMethod]
+        public void DontKnowWhatImDoing()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = testList.Count + 3;
+            int actual;
+
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
         // what happens to the last-added item?
+        [TestMethod]
+        public void DontKnowWhatImDoing()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = 77;
+            int actual;
+
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(77);
+            actual = testList[testList.Count - 1];
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
         // what happens to the Count?
+        [TestMethod]
+        public void DontKnowWhatImDoing()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int expected = testList.Count + 1;
+            int actual;
+
+            testList.Add(0);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
         // what happens if you add more items than the initial Capacity of the CustomList?
 
     }
 }
-}
+
